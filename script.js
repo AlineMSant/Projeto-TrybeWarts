6e6630213cbbf556a4763f7021cae4bdf3953593
Patrícia <ppatriciasouza01@gmail.com>
2022-11-17 16:11:41 -0300

    requisito tres ok

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnSignIn = document.getElementById('btn-sign-in');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+btnSignIn.addEventListener('click', () => {
+  if (email.value == 'tryber@teste.com' && password.value == '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
