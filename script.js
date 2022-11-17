3ccb024006d32ec15900843cfdf61629ea7ddcd4
Patrícia <ppatriciasouza01@gmail.com>
2022-11-17 18:06:38 -0300

    acerto linha 6 js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 
 btnSignIn.addEventListener('click', () => {
-  if (email.value == 'tryber@teste.com' && password.value == '123456') {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
